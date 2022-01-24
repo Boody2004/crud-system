596a2bc43de425a60e130ecf356087d3c26f38c6
Abdelrahman Atef <boodyatef665@gmail.com>
2022-01-24 22:38:14 +0200

    add function from getTotal

---
+++ app.js
@@ -0,0 +1,22 @@
+// get input inputs
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let submit = document.getElementById("submit");
+
+// get total
+function getTotal() {
+  if (price.value != "") {
+    let result = +price.value + +taxes.value + +ads.value - +discount.value;
+    total.innerHTML = result;
+    total.style.background = "var(--third-color)";
+  } else {
+    total.innerHTML = "0";
+    total.style.background = "var(--main-color)";
+  }
+}
